bd953f224b79f4b6abcdb85b12e4d4549da8a359
Trung Khiêm <trungkhiem1412@gmail.com>
2022-07-11 20:01:50 +0700

    update

---
+++ footer.php
@@ -0,0 +1,65 @@
+<footer class="container-fluid footer p-3">
+        <!-- danh mục -->
+        <div class="row">
+
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="title-col">
+                    CATEGORIES
+                </div>
+                <div class="item-col">
+                    <a href="#">INTERIOR</a>
+                    <a href="#">EXTERIOR</a>
+                    <a href="#">LIGHTING</a>
+                    <a href="#">PERFORMANCE</a>
+                    <a href="#">WHEELS & TIRES</a>
+                </div>
+            </div>
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="title-col">
+                    CUSTOMER
+                </div>
+                <div class="item-col">
+                    <a href="#">About us</a>
+                    <a href="#">Contact us</a>
+                    <a href="#">Payment pocily</a>
+                    <a href="#">My cart</a>
+                    <a href="#">Wishlist</a>
+                </div>
+            </div>
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="title-col">
+                    FOLLOW US
+                </div>
+                <div class="item-col">
+                    <a href="#">Instagram</a>
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Linkedin</a>
+                    <a href="#">Google +</a>
+                </div>
+            </div>
+            <div class="col-6 col-sm-6 col-md-3">
+                <div class="title-col">
+                    CONTACT INFO
+                </div>
+                <div class="item-col">
+                    <span>
+                        Phone:
+                        <a href="tel:123333333">
+                            +84 111222333
+                        </a>
+                    </span>
+                    <span>
+                        Email:
+                        <a href="mail:111222333@gmail.com">
+                            info@shore.com
+                        </a>
+                    </span>
+                    <span>
+                        401 BROADWAY, 24TH FLOOR
+                        ORCHARD VIEW, LONDON, UK
+                    </span>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,119 @@
+<header class="header fixed-top">
+        <!-- Top header -->
+        <div class="top-header d-none d-lg-block">
+            <div class="d-flex justify-content-between p-2 bg-success text-white text-uppercase">
+                <div class="d-flex gap-3">
+                    <div>Chi nhánh</div>
+                    <div>Theo dõi đơn hàng</div>
+                </div>
+                <div>không ngại va chạm</div>
+                <div class="d-flex gap-3">
+                    <div>Liên hệ</div>
+                    +84 9999999999
+                </div>
+            </div>
+        </div>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-md bg-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <!-- test -->
+                <div class="collapse navbar-collapse navbar-collapse-1">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Trang Chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cửa Hàng</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sản Phẩm</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Page</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Logo -->
+                <a class="navbar-brand" href="#">
+                    <img src="images/logo-b.png" alt="">
+                </a>
+                <!-- Navbar right -->
+                <div class="navbar-right">
+                    <ul class="d-flex navbar-nav me-auto mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link fs-4" href="#">
+                                <i class="bi bi-search"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item d-none d-md-block">
+                            <a tabindex="0" class="nav-link fs-4" href="#" data-bs-toggle="popover" data-bs-placement="top" data-bs-trigger="focus" title="Popover title" data-bs-content="Here's some amazing content.">
+                                <i class="bi bi-person-fill"></i>
+                            </a>
+                        </li>
+                        <!-- Yêu thích -->
+                        <!-- <li class="nav-item d-none d-md-block">
+                            <a class="nav-link fs-4" href="#">
+                                <i class="bi bi-suit-heart position-relative">
+                                    <span
+                                        class="position-absolute bottom-0 start-100 translate-middle badge rounded-pill bg-black fw-normal"
+                                        style="font-size: 10px;">
+                                        2
+                                    </span>
+                                </i>
+                            </a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a tabindex="0" class="nav-link fs-4" href="#" data-bs-toggle="popover" data-bs-placement="right" data-bs-trigger="focus" title="Popover title" data-bs-content="Here's some amazing content.">
+                                <i class="bi bi-cart position-relative">
+                                    <span
+                                        class="position-absolute bottom-0 start-100 translate-middle badge rounded-pill bg-black fw-normal"
+                                        style="font-size: 10px;">
+                                        1
+                                    </span>
+                                </i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Navbar menu -->
+                <div class="collapse navbar-collapse navbar-collapse-2" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Trang Chủ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cửa Hàng</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sản Phẩm</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Page</a>
+                        </li>
+                        <li class="nav-item muti-nav">
+                            <div class="box-1">
+                                <a class="nav-link" href="#">
+                                    <i class="bi bi-person-fill"></i> Đăng Nhập
+                                </a>
+                            </div>
+                            |
+                            <div class="box-2">
+                                <a class="nav-link" href="#">
+                                    <i class="bi bi-shield-lock-fill"></i> Đăng Ký
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
